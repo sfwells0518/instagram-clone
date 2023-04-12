7c3d90f5ab46d2ab55fb7e50ed3aab474baf5335
Sam Wells <sfwells0518@gmail.com>
2023-04-12 16:42:07 -0500

    add User REST routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # IG home
   get "/home" => "home#index"
+  # User routes: User auth and registration
+  get "/signup" => "users#new", as: "signup"
+  get "users/:id" => "users#show", as: "user"
+  get "users/:id/edit" => "users#edit", as: "edit_user"
+  post "/users" => "users#create"
+  patch "users/:id" => "users#update"
 end
