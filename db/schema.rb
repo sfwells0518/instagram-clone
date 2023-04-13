82fbeaafb5215d1132b90b072ee6cc184702d174
Sam Wells <sfwells0518@gmail.com>
2023-04-13 14:17:07 -0500

    run migration to remove location attribute from User model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_191212) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_191557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_191212) do
     t.string "username"
     t.string "email_address"
     t.string "date_of_birth"
-    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
