ee3cbd3cc6dfa6275974ab57a5905697c305f75e
Sam Wells <sfwells0518@gmail.com>
2023-04-13 14:05:26 -0500

    add action for new in Users controller and create view erb template for new user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,29 @@
 Rails.application.routes.draw do
   # IG home
-  get "/home" => "home#index"
+
+  # get "/home" => "home#index"
+
   # User routes: User auth and registration
+
   get "/signup" => "users#new", as: "signup"
-  get "/users/:id" => "users#show", as: "user"
-  get "/users/:id/edit" => "users#edit", as: "edit_user"
-  post "/users" => "users#create"
-  patch "/users/:id" => "users#update"
-  # Session routes: Session management
-  get "/login" => "sessions#new", as: "login"
-  post "/sessions" => "sessions#create"
-  delete "/logout" => "sessions#destroy", as: "logout"
-  # Post routes: Post management
-  get "/posts" => "posts#index", as: "posts"
-  get "/posts/new" => "posts#new", as: "new_post"
-  get "/posts/:id" => "posts#show", as: "post"
-  get "/posts/:id/edit" => "posts#edit", as: "edit_post"
-  post "/posts" => "posts#create"
-  patch "/posts/:id" => "posts#update"
-  delete "/posts/:id" => "posts#destroy"
+  # get "/users/:id" => "users#show", as: "user"
+  # get "/users/:id/edit" => "users#edit", as: "edit_user"
+  # post "/users" => "users#create"
+  # patch "/users/:id" => "users#update"
+
+  # # Session routes: Session management
+
+  # get "/login" => "sessions#new", as: "login"
+  # post "/sessions" => "sessions#create"
+  # delete "/logout" => "sessions#destroy", as: "logout"
+
+  # # Post routes: Post management
+
+  # get "/posts" => "posts#index", as: "posts"
+  # get "/posts/new" => "posts#new", as: "new_post"
+  # get "/posts/:id" => "posts#show", as: "post"
+  # get "/posts/:id/edit" => "posts#edit", as: "edit_post"
+  # post "/posts" => "posts#create"
+  # patch "/posts/:id" => "posts#update"
+  # delete "/posts/:id" => "posts#destroy"
 end
