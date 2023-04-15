cbd8b142794cab8927bfc6dfee86c5f8a025bc14
Sam Wells <sfwells0518@gmail.com>
2023-04-14 23:56:25 -0500

    adjust users controller code to define user params at the top and remove private. successful testing of POST create action to create a new user and save to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  # Define user_params method first
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email_address,
+      :date_of_birth,
+      :username,
+      :password,
+      :confirm_password
+    )
+  end
+
+  # Define new and create actions after user_params
   def new
     @user = User.new
   end
@@ -7,23 +22,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      render json: { message: "user created successfully!" }
+      # redirect_to root_path
     else
       render json: { message: "failed to save, try submitting again!" }
     end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(
-      :first_name,
-      :last_name,
-      :email_address,
-      :username,
-      :date_of_birth,
-      :password,
-      :confirm_password
-    )
   end
 end
