99ab530c75bbdfcd5e66bfce2bdf69193eda66ab
Sam Wells <sfwells0518@gmail.com>
2023-04-14 22:55:20 -0500

    add has many associations to User model for posts, comments, likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :likes
 
   # Specify the allowed attributes and their types using the `attribute` method:
   attribute :first_name, :string
