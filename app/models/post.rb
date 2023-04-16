80bd3aa016f461f0958560ee54b75845fc3eced8
Sam Wells <sfwells0518@gmail.com>
2023-04-16 16:06:02 -0500

    add associations and attributes to Post model post.rb file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  # Specify the allowed attributes and their types using the `attribute` method:
+  attribute :user_id, :integer
+  attribute :caption, :string
+  attribute :image, :string
 end
