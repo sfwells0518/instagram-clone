5ad66b7ab6b03215f1879ad8b2959c50f988a11c
Sam Wells <sfwells0518@gmail.com>
2023-04-17 15:52:59 -0500

    add user_id column to Profile model/table via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_205018) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_205212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_205018) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
