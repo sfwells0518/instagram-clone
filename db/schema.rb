58671881d650b2ead81cd41248eb8a2819ca5cd1
Sam Wells <sfwells0518@gmail.com>
2023-04-17 15:51:01 -0500

    add Profile model with profile_image and bio attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_204801) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_205018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_204801) do
     t.string "image"
     t.text "caption"
     t.text "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "profile_image"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
