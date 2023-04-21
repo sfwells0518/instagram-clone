d88e9c822b2c8658e6c10a964e6b5a056cabb82e
Sam Wells <sfwells0518@gmail.com>
2023-04-21 15:51:18 -0500

    add update action to profiles controller and complete successful initial test

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,18 @@
 class ProfilesController < ApplicationController
+  def update
+    # @profile = Profile.where(user_id: current_user.id)
+    # @profile.profile_image = params[:profile_image] || @profile.profile_image
+    # @profile_bio = params[:bio] || @profile_bio
+
+    # # save updated profile
+
+    # if @profile.save
+    #   render :show
+    # else
+    #   render json: { errors: @profile.errors.full_messages }
+    # end
+
+    # initial test code:
+    # render json: { message: "welcome to profile update test" }
+  end
 end
